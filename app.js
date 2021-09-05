fa8882ac477d10156a5842955236a361697c1a09
glennbech <glenn.bech@gmail.com>
2021-09-05 19:21:06 +0200

    Users

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const http = require('http');
 
-                     const port = process.env.APP_PORT;
+const port = process.env.APP_PORT;
 
-                     const server = http.createServer((req, res) => {
-                       res.statusCode = 200;
-                       res.setHeader('Content-Type', 'application/json');
-                       var user = {
-                         first_name: "John",
-                         last_name: "Smith",
-                         age: "38",
-                         department: "Software"
-                         };
-                       res.end(JSON.stringify(user));
-                     });
+const server = http.createServer((req, res) => {
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'application/json');
+   var user = {
+     first_name: "John",
+     last_name: "Smith",
+     age: "38",
+     department: "Software"
+     };
+   res.end(JSON.stringify(user));
+ });
 
-                     server.listen(port,  () => {
-                       console.log(`Server running` );
-                     });
+ server.listen(port,  () => {
+   console.log(`Server running` );
+ });
