b8571cad444a923872c72726a70e4fefde808a39
glennbech <glenn.bech@gmail.com>
2021-09-12 23:21:05 +0200

    New Instance ami

--- infra/classroom/classroom.tf
+++ infra/classroom/classroom.tf
@@ -1,6 +1,6 @@
 module "students" {
   source          = "../seat"
-  ami             = "ami-0cb2d4aa3008821f4"
+  ami             = "ami-08a3bbf30d1f2dac7"
   for_each        = var.students
   student_id      = each.key
   student_keybase = each.value.keybase_id
