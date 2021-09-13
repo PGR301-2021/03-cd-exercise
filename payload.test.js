2ad33483423ba9c4fa3d1a476b79982f1a0b04fe
glennbech <glenn.bech@gmail.com>
2021-09-13 08:11:26 +0200

    Fix the test

--- payload.test.js
+++ payload.test.js
@@ -5,5 +5,5 @@ test("Creates at least age correctly in payload", () => {
 });
 
 test("Creates at least age correctly in payload", () => {
-    expect(payload("glenn", "Bech", 45, "DevOps").age).toBe(90);
+    expect(payload("glenn", "Bech", 45, "DevOps").age).toBe(45);
 });
