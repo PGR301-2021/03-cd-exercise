dbcbd819c8868e62105cf6f2a06332e5cd182631
glennbech <glenn.bech@gmail.com>
2021-09-14 10:20:27 +0200

    stupido 0

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const payload = require('./payload');
 
-
+const port = 3000;
 
 const server = http.createServer((req, res) => {
    res.statusCode = 200;
    res.setHeader('Content-Type', 'application/json');
-   res.end(JSON.stringify(payload("Glenn", "Bech", 44, "DevOps")));
+   res.end(JSON.stringify(payload("Glenn", "Bech", 45, "DevOps")));
  });
 
  server.listen(port,  () => {
